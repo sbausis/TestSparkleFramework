174e4a222139d22086f4ffc75d495c465215c17e
sbausis <sbausis@gmx.net>
2014-10-22 03:20:56 +0200

    Initial Commit

---
+++ TestSparkleFramework/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestSparkleFramework
+//
+//  Created by Simon Pascal Baur on 22/10/14.
+//  Copyright (c) 2014 Simon Pascal Baur. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TestSparkleFramework/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TestSparkleFramework
+//
+//  Created by Simon Pascal Baur on 22/10/14.
+//  Copyright (c) 2014 Simon Pascal Baur. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
